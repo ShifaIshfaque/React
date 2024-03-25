0a42df2b99c7544bf188ccf35de1f158867a2693
Shifa <shifa.ishfaque@gmail.com>
2024-03-26 00:42:27 +0500

    Counter project with updates

---
+++ counterProject/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  let [counter, setCounter] = useState(10)
+
+  const addValue = () =>{
+    if(counter < 34){
+    setCounter(counter + 1)
+  }};
+
+  const removeValue = () =>{
+    if(counter > 0 ){
+    setCounter(counter - 1)
+  }};
+  return (
+    <>
+     <h1>My First Counter Project</h1>
+     <h2>Counter Value : {counter}</h2>
+     <button onClick={addValue}>Add Value : {counter}</button>
+     <button onClick={removeValue}>Remove Value : {counter}</button>
+     <p>Footer: {counter}</p>
+       
+    </>
+  )
+}
+
+export default App
