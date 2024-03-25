eec5ef7ea4cfb333dc9fff8c6875d0e618c0f016
Shifa <shifa.ishfaque@gmail.com>
2024-03-26 01:03:18 +0500

    update

--- counterProject/src/App.jsx
+++ counterProject/src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 
 function App() {
  
-  let [counter, setCounter] = useState(10)
+  let [counter, setCounter] = useState(7)
 
   const addValue = () =>{
-    if(counter < 34){
+    if(counter < 9){
     setCounter(counter + 1)
   }};
 
@@ -22,7 +22,7 @@ function App() {
      <h2>Counter Value : {counter}</h2>
      <button onClick={addValue}>Add Value : {counter}</button>
      <button onClick={removeValue}>Remove Value : {counter}</button>
-     <p>Footer: {counter}</p>
+     {/* <p>Footer: {counter}</p> */}
        
     </>
   )
